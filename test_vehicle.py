62e5857e31addcc6c28a613074cecf59fd7d278e
Duck <mvch@triangle.software>
2023-04-04 12:50:01 +0300

    a new test case

--- test_vehicle.py
+++ test_vehicle.py
@@ -5,9 +5,11 @@ from models import Vehicle
 class TestVehicle(unittest.TestCase):
 
     def test_move(self):
-        v8 = Vehicle('truck')
+        # TODO: does vehicle speed depends on something?
+        # TODO: add a argument to the constructor
+        v8 = Vehicle('camaro')
         v8.move(3)
-        self.assertEqual(4, v8.position)
+        self.assertEqual(25, v8.position)
 
 
 # entry point
