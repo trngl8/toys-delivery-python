52f27cb8dbc71120a05a6abe4dd195d34caa3bde
Duck <mvch@triangle.software>
2023-04-04 16:10:52 +0300

    TODO changes

--- test_vehicle.py
+++ test_vehicle.py
@@ -5,8 +5,7 @@ from models import Vehicle
 class TestVehicle(unittest.TestCase):
 
     def test_move(self):
-        # TODO: does vehicle speed depends on something?
-        # TODO: add a argument to the constructor
+        # TODO: vehicle speed must depends on speed argument
         v8 = Vehicle('camaro')
         v8.move(3)
         self.assertEqual(25, v8.position)
