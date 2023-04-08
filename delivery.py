579df12f17108a2e0a578514c5b2678c78d88a44
Duck <mvch@triangle.software>
2023-04-08 19:33:26 +0300

    fix client code

--- delivery.py
+++ delivery.py
@@ -1,6 +1,6 @@
 from models import Courier
 from models import Vehicle
-from models import Childrenlist
+from models import DeliveryList
 from models import Partner
 
 
@@ -8,8 +8,7 @@ obj = Courier('USSR')
 v8 = Vehicle('truck')
 print(f'Name: {obj.get_name()}, can work: {obj.can_work()}')
 
-child1 = Childrenlist(1, 'Vasya', 'Bashirkastan', 3)
-child2 = Childrenlist(2, 'Olya', 'Vladivostok', 2)
-child3 = Childrenlist(3, 'Sanya', 'Moskva', 1)
+child_list = DeliveryList()
+child_list.add('Theo', '7172', 3)
 
-grand_child = Partner("Snegurochka", child1)
+courier_partner = Partner("Snegurochka", child_list)
