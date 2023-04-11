56e8cdcb88420b61a9a7792e559b9a10e127e3ac
Bogdan <fedyna21@gmail.com>
2023-04-11 20:07:27 +0300

    add cape_color as parameter

--- test_courier.py
+++ test_courier.py
@@ -5,12 +5,12 @@ from models import Courier
 class TestCourier(unittest.TestCase):
 
     def test_move(self):
-        glovo = Courier("kubernetes", 'Santa')
+        glovo = Courier("kubernetes", 'Santa', 'red')
         result = glovo.delivery("fast")
         self.assertEqual(True, result)
 
     def test_name(self):
-        target = Courier("test1", "test2")
+        target = Courier("test1", "test2", 'test3')
         result = target.get_name()
         self.assertEqual("test2", result)
 
