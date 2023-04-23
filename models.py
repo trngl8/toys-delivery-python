fd3618fc4a066aeda37c1244f6174e86a8232fe6
Arthur Kravchenko <arturkra92@gmail.com>
2023-04-23 16:05:03 +0300

    fix problems

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class Courier:
         return self.__name
 
     def get_full_name(self):
-        return f"{self.__name} {self.__cape_color}"
+        return f"{self.__cape_color} {self.__name}"
 
     def get_partners_count(self):
         if self.__culture == 'USSR':
