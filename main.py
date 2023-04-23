bb02ffe1ee97cffb979d5b6afe592de05c69d100
Arthur Kravchenko <arturkra92@gmail.com>
2023-04-23 15:08:14 +0300

    add fullname to courier

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 
+def fib(n):
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+
+for i in range(3):
+    print(fib(i))
