dc1346b08572c6a18f19001f67441e6e0acc5621
Artur Kravchenko <87659503+ACraft4@users.noreply.github.com>
2023-04-29 13:35:09 +0300

    close #56 Santas transport (#73)

--- models.py
+++ models.py
@@ -96,9 +96,13 @@ class Sledge:
         self.v_type = v_type
         self.speed = 150
         self.position = 1
+        self.deer_names = ["Dasher", "Dancer", "Prancer", "Vixen", "Cupid", "Donner", "Blitzen", "Comet", "Rudolph"]
 
     def move(self, position):
         self.position += position
+
+    def get_engine(self):
+        return self.deer_names
 
 
 class Partner:
--- test_vehicle.py
+++ test_vehicle.py
@@ -1,5 +1,6 @@
 import unittest
 from models import Vehicle
+from models import Sledge
 
 
 class TestVehicle(unittest.TestCase):
@@ -8,6 +9,14 @@ class TestVehicle(unittest.TestCase):
         v8 = Vehicle('camaro', 8)
         v8.move(3)
         self.assertEqual(25, v8.position)
+
+    def test_sledge(self):
+        expected_deer_names = ["Dasher", "Dancer", "Prancer", "Vixen", "Cupid", "Donner", "Blitzen", "Comet", "Rudolph"]
+
+        sledge = Sledge('sled')
+        result = sledge.get_engine()
+        self.assertEqual(9, len(result))
+        self.assertEqual(expected_deer_names, result)
 
 
 # entry point
